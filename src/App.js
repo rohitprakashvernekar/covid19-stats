6fa76b7df64afd2f64463ee68b55b5472d44b7d1
rohitvernekar7 <rohitvernekar7@yahoo.com>
2021-02-17 19:03:34 +0530

    fetched api and added search box

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import "./App.css";
+import CountryList from "./components/CountryList/CountryList";
+import SearchBox from "./components/SearchBox/SearchBox";
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      countries: [],
+      stats: [],
+      searchField: "",
+    };
+  }
+
+  async componentDidMount() {
+    const response = await fetch("https://api.covid19api.com/countries");
+    const countries = await response.json();
+    this.setState({ countries });
+    this.state.countries.forEach(async (country) => {
+      const response = await fetch(
+        `https://api.covid19api.com/total/country/${country.Slug}`
+      );
+      const data = await response.json();
+      if (data.length)
+        this.setState((prevState) => ({
+          stats: prevState.stats.concat({
+            ...data[data.length - 1],
+            CountryCode: country.ISO2,
+          }),
+        }));
+    });
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
+  render() {
+    const { stats, searchField } = this.state;
+    const filteredCountries = stats.filter((country) =>
+      country.Country.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className="App">
+        <SearchBox
+          placeholder="Enter country name..."
+          handleChange={this.handleChange}
+        />
+        <CountryList stats={filteredCountries} />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/CountryList/CountryList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Country from '../Country/Country';
+import './countrylist.css';
+
+const CountryList = ({stats}) => {
+  return (
+    <div className='countrylist'>
+      {stats.map(country => 
+        <Country key={country.CountryCode} stats = {country}/>
+      )}
+    </div>
+  );
+};
+
+export default CountryList;
