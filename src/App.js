fff379ff5b55dac62ec8b02672c7db4ff4faf43d
rohitvernekar7 <rohitvernekar7@yahoo.com>
2021-02-18 12:10:52 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,55 +1,46 @@
-import React from "react";
-import "./App.css";
-import CountryList from "./components/CountryList/CountryList";
-import SearchBox from "./components/SearchBox/SearchBox";
-
-class App extends React.Component {
-  constructor() {
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CountryList from './components/CountryList/CountryList';
+import SearchBox from './components/SearchBox/SearchBox';
+class App extends React.Component{
+  constructor(){
     super();
     this.state = {
-      countries: [],
-      stats: [],
-      searchField: "",
-    };
+     countries:[],
+     stats:[],
+     searchField:''
+    }
   }
-
-  async componentDidMount() {
-    const response = await fetch("https://api.covid19api.com/countries");
-    const countries = await response.json();
-    this.setState({ countries });
-    this.state.countries.forEach(async (country) => {
-      const response = await fetch(
-        `https://api.covid19api.com/total/country/${country.Slug}`
-      );
-      const data = await response.json();
-      if (data.length)
-        this.setState((prevState) => ({
-          stats: prevState.stats.concat({
-            ...data[data.length - 1],
-            CountryCode: country.ISO2,
-          }),
-        }));
-    });
+  async componentDidMount(){
+    const resp = await fetch('https://api.covid19api.com/countries')
+    const countries = await resp.json()
+    this.setState({countries})
+    this.state.countries.forEach(async country => {
+      const resp = await fetch(`https://api.covid19api.com/total/country/${country.Slug}`)
+      const data = await resp.json()
+      if(data.length)
+      this.setState(prevState => (
+        {stats:prevState.stats.concat({...data[data.length - 1],CountryCode:country.ISO2})}
+      ))
+    })
   }
-
-  handleChange = (e) => {
-    this.setState({ searchField: e.target.value });
-  };
-
-  render() {
-    const { stats, searchField } = this.state;
-    const filteredCountries = stats.filter((country) =>
+  handleChange = (e) =>{
+    this.setState({searchField:e.target.value})
+  }
+  render(){
+    const {stats,searchField} = this.state
+    const filteredCountries = stats.filter(country =>(
       country.Country.toLowerCase().includes(searchField.toLowerCase())
-    );
-    return (
+    ))
+    return(
       <div className="App">
-        <SearchBox
-          placeholder="Enter country name..."
-          handleChange={this.handleChange}
-        />
-        <CountryList stats={filteredCountries} />
-      </div>
-    );
+        <h1>Covid19 Stats Web App</h1>
+        <SearchBox placeholder="Enter country name ..." handleChange={this.handleChange}/>
+        <CountryList stats = {filteredCountries}/>
+    </div>
+    )
   }
 }
+
 export default App;
